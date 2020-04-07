4dea491debe49a3392e78631329ae3e35243e663
Alejandro Sherman <asher011@wch136-16.cs.ucr.edu>
2020-04-06 19:08:40 -0700

    added basic main

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: asher011
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *  Partner(s) Name: N/A
+ *	Lab Section: 23
+ *	Assignment: Lab #2  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -14,10 +14,12 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRB = 0xFF;
+    PORTB = 0x00;
 
     /* Insert your solution below */
     while (1) {
-
+            PORTB = 0x0F;
     }
     return 1;
 }
