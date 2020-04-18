87d5d4992f1b69cdb8773d46d3893516165e5aa2
AlejandroSherman <alejandro.sherman100@gmail.com>
2020-04-18 15:29:29 -0700

    completed part 1

---
+++ Lab5_light_display/turnin/asher011_lab5_part1.c
@@ -0,0 +1,51 @@
+/*	Author: asher011
+ *  Partner(s) Name:
+ *	Lab Section: 23
+ *	Assignment: Lab #5  Exercise #1
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+unsigned char GetBit(char port, char bit_position) {
+	return ( (port >> bit_position) & 0x01 );
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;//input
+    DDRC = 0xFF; PORTC = 0x00;//output
+    /* Insert your solution below */
+    unsigned char fuelLev = 0x00;
+    while (1) {
+    fuelLev = ((~PINA) & 0x0F);
+		if ((fuelLev >= 0x00) && (fuelLev <= 0x01)) {
+				PORTC = 0x40;
+		}
+    if ((fuelLev >= 0x01) && (fuelLev <= 0x02)) {
+				PORTC = 0x60;
+		}
+    if ((fuelLev >= 0x03) && (fuelLev <= 0x04)) {
+				PORTC = 0x70;
+		}
+    if ((fuelLev >= 0x05) && (fuelLev <= 0x06)) {
+				PORTC = 0x38;
+		}
+    if ((fuelLev >= 0x07) && (fuelLev <= 0x09)) {
+				PORTC = 0x3C;
+		}
+    if ((fuelLev >= 0x0A) && (fuelLev <= 0x0C)) {
+				PORTC = 0x3E;
+		}
+    if ((fuelLev >= 0x0D) && (fuelLev <= 0x0F)) {
+				PORTC = 0x3F;
+		}
+
+    }
+    return 1;
+}
